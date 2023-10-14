0918efbdbc0b52b635d47b18f39bc331a6aac1bf
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-10-14 12:15:33 +0700

    contact 0.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
-import * as React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import router from './useRouteElement'
+import "./App.css";
+import * as React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./useRouteElement";
 function App() {
   const [showButton, setShowButton] = React.useState(false);
 
@@ -15,50 +15,90 @@ function App() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   const goTop = () =>
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
+      behavior: "smooth",
+    });
+
+  const telCall = () => {
+    window.location.href = "tel:0971559971";
+  };
+
   return (
     <React.StrictMode>
       <RouterProvider router={router} />
       {showButton && (
-        <button className={` fixed p-2 rounded-md bottom-5 right-5 bg-red-600`} onClick={() => goTop()}>
+        <button
+          className={` fixed p-2 rounded-md bottom-5 right-5 bg-red-600`}
+          onClick={() => goTop()}
+        >
           <svg
-            width='30px'
-            height='30px'
-            viewBox='0 0 24 24'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-            stroke='#f1f1f1f1'
-            transform='matrix(1, 0, 0, 1, 0, 0)'
+            width="30px"
+            height="30px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#f1f1f1f1"
+            transform="matrix(1, 0, 0, 1, 0, 0)"
           >
-            <g id='SVGRepo_bgCarrier' stroke-width='0' />
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
-            <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round' />
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
 
-            <g id='SVGRepo_iconCarrier'>
-              {' '}
+            <g id="SVGRepo_iconCarrier">
+              {" "}
               <path
-                d='M12 6V18M12 6L7 11M12 6L17 11'
-                stroke='#f1f1f1f1'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-              />{' '}
+                d="M12 6V18M12 6L7 11M12 6L17 11"
+                stroke="#f1f1f1f1"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />{" "}
             </g>
           </svg>
         </button>
       )}
+      {
+        <a
+          className={`fixed p-2 bottom-3 left-40`}
+          href="https://zalo.me/0971559971"
+          style={{ left: "9rem" }}
+        >
+          <img
+            src="https://lh3.googleusercontent.com/X7JUHmDCeT845_MgAttYi_F8RsiHQr9ufnRqk2SiyiJZ1KBZgZek9wFOMuOwDuy4leA=w47"
+            alt="0906215668"
+            style={{ borderRadius: "50%" }}
+          />
+        </a>
+      }
+      {
+        <button
+          className={` fixed p-2 rounded-md bottom-5 ml-10 bg-green-500 text-white hover:bg-green-600`}
+          onClick={() => telCall()}
+          style={{ borderRadius: "25px" }}
+        >
+          <div
+            className="fixed p-2 rounded-md bottom-5 ml-10 bg-green-500 text-white hover:bg-green-600"
+            style={{ borderRadius: "50%" }}
+          ></div>
+          <a href="tel:0971559971" className="font-semibold">
+            0971559971
+          </a>
+        </button>
+      }
     </React.StrictMode>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,134 @@
-const Contact = () => {
-  return <div>Contact</div>
-}
+import Banner from "~/assets/images/Banner-lien-he.jpg";
 
-export default Contact
+const Contact = () => {
+  return (
+    <div className="container">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="col-span-2 md:col-span-1">
+          <img
+            src={Banner}
+            className="w-full mb-3"
+            alt="Banner Liên Hệ"
+          />
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <img
+            src="/images/icon-lien-he.png"
+            className="mb-2"
+            alt="Icon Liên Hệ"
+          />
+          <h3 className="font-bold text-primary text-2xl">
+            LIÊN HỆ VỚI CHÚNG TÔI
+          </h3>
+          <p className="text-muted my-3">
+            Hãy để lại thông tin, chuyên viên của chúng tôi sẽ nhanh chóng hỗ
+            trợ bạn.
+          </p>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-input bg-light mb-4"
+              id="hoten"
+              placeholder="Họ và Tên"
+            />
+            <input
+              type="text"
+              className="form-input bg-light mb-4"
+              id="txtdt"
+              placeholder="Số điện thoại"
+            />
+            <input
+              type="email"
+              className="form-input bg-light mb-4"
+              id="txtemail"
+              placeholder="Email"
+            />
+            <textarea
+              className="form-input bg-light mb-4"
+              placeholder="Nội dung"
+              id="txtnoidung"
+              rows={5}
+              defaultValue={""}
+            />
+          </div>
+          <div className="text-center">
+            <a role="button" id="btnclick">
+              <img
+                src="/images/bt-lien-he.png"
+                alt="Button Liên Hệ"
+                className="w-40"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col-span-2 md:col-span-1">
+          <h4 className="font-bold text-primary text-2xl mb-4">
+            CÔNG TY DỊCH VỤ KẾ TOÁN ANPHA
+          </h4>
+          <div className="flex mb-2">
+            <div className="badge rounded-full bg-light avatar-comment me-2">
+              <i className="fa text-secondary fa-phone" aria-hidden="true" />
+            </div>
+            <div>
+              <p className="text-primary font-bold mb-2">DỊCH VỤ PHÁP LÝ</p>
+              <p className="mb-2">
+                <small className="text-lg">
+                  <b>Miền Bắc:</b> <a href="tel:0984477711">098 44 777 11</a> |{" "}
+                  <b>Miền Trung:</b> <a href="tel:0903003779">0903 003 779</a> |{" "}
+                  <b>Miền Nam:</b> <a href="tel:0938268123">0938 268 123</a>
+                </small>
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="badge rounded-full bg-light avatar-comment me-2">
+              <i className="fa text-secondary fa-phone" aria-hidden="true" />
+            </div>
+            <div>
+              <p className="text-primary font-bold mb-2">DỊCH VỤ KẾ TOÁN</p>
+              <p className="mb-2">
+                <small className="text-lg">
+                  <b>Miền Bắc:</b> <a href="tel:0901042555">0901 042 555</a> |{" "}
+                  <b>Miền Trung:</b> <a href="tel:0939356866">0939 35 6866</a> |{" "}
+                  <b>Miền Nam:</b> <a href="tel:0902602345">0902 60 2345</a>
+                </small>
+              </p>
+            </div>
+          </div>
+          <div className="mb-3">
+            <div className="badge rounded-full bg-light avatar-comment me-2">
+              <i className="fa text-secondary fa-envelope" aria-hidden="true" />
+            </div>
+            <small className="text-lg">
+              <b>cskh@ketoananpha.vn</b>
+            </small>
+            <div className="badge rounded-full bg-light avatar-comment mx-2">
+              <i
+                className="fab text-secondary fa-facebook-f"
+                aria-hidden="true"
+              />
+            </div>
+            <small className="text-lg">
+              <b>
+                <a href="//fb.com/ketoananpha" target="_blank" rel="nofollow">
+                  fb.com/ketoananpha
+                </a>
+              </b>
+            </small>
+          </div>
+          <div className="ratio ratio-21/9">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7839.106495025981!2d106.690436!3d10.768873!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb6de62968c62a08d!2zROG7i2NoIFbhu6UgVGjDoG5oIEzhuq1wIEPDtG5nIFR5IC0gMjUwLjAwMMSRIC0gS-G6vyBUb8OhbiBBbnBoYQ!5e0!3m2!1svi!2sus!4v1636899320143!5m2!1svi!2sus"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              title="Google Maps"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
